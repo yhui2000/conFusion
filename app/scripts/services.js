952fdd8ccbaa5a00480e4b8f2e3bbad1ee796ac1
Andre Henrique <andre_dk@hotmail.com>
2015-12-26 00:26:43 -0200

    Week 3, ngRoute and Single Page Applications

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -4,6 +4,7 @@ angular.module('confusionApp')
 	.factory('menuFactory', function(){
 		var menufac = {};
 		var dishes = [{
+	        _id:0,
 	        name:'Uthapizza',
 	        image: 'images/uthapizza.png',
 	        category: 'mains',
@@ -37,6 +38,7 @@ angular.module('confusionApp')
 				date:"2011-12-02T17:57:28.556094Z"
 	        }]
 	      },{
+	        _id:1,
 	        name:'Zucchipakoda',
 	        image: 'images/zucchipakoda.png',
 	        category: 'appetizer',
@@ -70,6 +72,7 @@ angular.module('confusionApp')
 				date:"2011-12-02T17:57:28.556094Z"
 	        }]
 	      },{
+	        _id:2,
 	        name:'Vadonut',
 	        image: 'images/vadonut.png',
 	        category: 'appetizer',
@@ -103,6 +106,7 @@ angular.module('confusionApp')
 				date:"2011-12-02T17:57:28.556094Z"
 	        }]
 	      },{
+	        _id:3,
 	        name:'ElaiCheese Cake',
 	        image: 'images/elaicheesecake.png',
 	        category: 'dessert',
